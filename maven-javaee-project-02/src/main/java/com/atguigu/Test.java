87c1abc8c54fdcf2fa3b2476659600f0107abb05
maoshuaiqi <maoshuaiqi@tenwa.com.cn>
2023-12-29 17:42:28 +0800

    测试合并1

--- maven-javaee-project-02/src/main/java/com/atguigu/Test.java
+++ maven-javaee-project-02/src/main/java/com/atguigu/Test.java
@@ -2,6 +2,6 @@ package com.atguigu;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("-----------测试提交-----------");
+        System.out.println("-----------测试提交1-----------");
     }
 }
