53a649d10b6116668af1e11c65918150360510ac
maoshuaiqi <maoshuaiqi@tenwa.com.cn>
2024-01-03 09:57:24 +0800

    develop提交

--- maven-javaee-project-02/src/main/java/com/atguigu/Test.java
+++ maven-javaee-project-02/src/main/java/com/atguigu/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("-----------测试提交1-----------");
         System.out.println("-----------测试提交2-----------");
+        System.out.println("-----------develop提交-----------");
     }
 }
