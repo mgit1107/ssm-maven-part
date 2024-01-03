93630dece2e6bb9add592be67d76c675ba70b56c
maoshuaiqi <maoshuaiqi@tenwa.com.cn>
2024-01-03 15:38:12 +0800

    删除

--- maven-javaee-project-02/src/main/java/com/atguigu/Test.java
+++ maven-javaee-project-02/src/main/java/com/atguigu/Test.java
@@ -2,5 +2,12 @@ package com.atguigu;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println("-----------测试提交1-----------");
+        System.out.println("-----------测试提交2-----------");
+        System.out.println("-----------develop提交-----------");
+        System.out.println("-----------master提交-----------");
+        System.out.println("-----------develop提交2-----------");
+        System.out.println("-----------master2提交-----------");
+        System.out.println("-----------master3提交-----------");
     }
 }
